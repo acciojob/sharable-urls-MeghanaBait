b0977f6674295b8664ff20bedee44a15279e86c4
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2024-01-31 10:14:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ document.getElementById('infoForm').addEventListener('submit', function(event) {
             event.preventDefault();
 
             // Get the values from the input fields
-            var name = document.getElementById('name').value;
-            var year = document.getElementById('year').value;
+            let name = document.getElementById('name').value;
+            let year = document.getElementById('year').value;
 
             // Create the query string
-            var queryString = `?name=${name}&year=${year}`;
+            let queryString = `?name=${name}&year=${year}`;
 
             // Update the h3 element with the new URL
-            document.getElementById('url').textContent = `https://localhost:8080/${queryString}`;
+            document.getElementById('url').innerHTML+= `${queryString}`;
         });
